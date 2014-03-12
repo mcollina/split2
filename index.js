3f98312a3abce55b0e4a4b7ae4911a093f926db6
Matteo Collina <matteo.collina@gmail.com>
2014-03-12 12:54:36 +0100

    API equivalence with split()

--- index.js
+++ index.js
@@ -2,18 +2,18 @@
 var through = require('through2')
 
 function transform(chunk, enc, cb) {
-  var list = chunk.split(this.separator)
+  var list = chunk.split(this.matcher)
     , remaining = list.pop()
     , i
 
   if (list.length >= 1) {
-    this.push(this._last + list.shift())
+    this.push(this.mapper((this._last + list.shift())))
   } else {
     remaining = this._last + remaining
   }
 
   for (i = 0; i < list.length; i++) {
-    this.push(list[i])
+    this.push(this.mapper(list[i]))
   }
 
   this._last = remaining
@@ -23,21 +23,38 @@ function transform(chunk, enc, cb) {
 
 function flush(cb) {
   if (this._last)
-    this.push(this._last)
+    this.push(this.mapper(this._last))
 
   cb()
 }
 
-function split(options) {
+function noop(incoming) {
+  return incoming
+}
+
+function split(matcher, mapper, options) {
+
+  if (typeof matcher === 'object' && !(matcher instanceof RegExp)) {
+    options = matcher
+    matcher = null
+  }
+
+  if (typeof matcher === 'function') {
+    mapper = matcher
+    matcher = null
+  }
 
   options = options || {}
   options.decodeStrings = false
-  options.encoding = 'utf8'
 
   var stream = through(options, transform, flush)
 
+  // this stream is in objectMode only in the readable part
+  stream._readableState.objectMode = true;
+
   stream._last = ''
-  stream.separator = options.separator || '\n'
+  stream.matcher = matcher || '\n'
+  stream.mapper = mapper || noop
 
   return stream
 }
--- test.js
+++ test.js
@@ -3,6 +3,7 @@ var test      = require('tap').test
   , split     = require('./')
   , callback  = require('callback-stream')
   , strcb     = callback.bind(null, { decodeStrings: false })
+  , objcb     = callback.bind(null, { objectMode: true })
 
 test('split two lines on end', function(t) {
   t.plan(1)
@@ -44,14 +45,56 @@ test('accumulate multiple writes', function(t) {
   input.end()
 })
 
-test('split using a custom char', function(t) {
+test('split using a custom string matcher', function(t) {
   t.plan(1)
 
-  var input = split({ separator: '~' })
+  var input = split('~')
 
   input.pipe(strcb(function(err, list) {
     t.deepEqual(list, ['hello', 'world'])
   }))
 
   input.end('hello~world')
+})
+
+test('split using a custom regexp matcher', function(t) {
+  t.plan(1)
+
+  var input = split(/~/)
+
+  input.pipe(strcb(function(err, list) {
+    t.deepEqual(list, ['hello', 'world'])
+  }))
+
+  input.end('hello~world')
+})
+
+test('support an option argument', function(t) {
+  t.plan(2)
+
+  var input = split({ highWatermark: 2 })
+
+  input.pipe(strcb(function(err, list) {
+    t.notOk(err, 'no errors')
+    t.deepEqual(list, ['hello', 'world'])
+  }))
+
+  input.end('hello\nworld')
+})
+
+test('support a mapper function', function(t) {
+  t.plan(2)
+
+  var a = { a: '42' }
+    , b = { b: '24' }
+  var input = split(JSON.parse)
+
+  input.pipe(objcb(function(err, list) {
+    t.notOk(err, 'no errors')
+    t.deepEqual(list, [a, b])
+  }))
+
+  input.write(JSON.stringify(a))
+  input.write('\n')
+  input.end(JSON.stringify(b))
 })
